bf28b0bfc113e3dfb1bca4d66b5791c4d0a1c691
Vidhya <vidhya.mtech@gmail.com>
2024-12-17 15:38:03 -0600

    updated skeleton 17

--- src/test/java/StepDefinition/RunnerTest.java
+++ src/test/java/StepDefinition/RunnerTest.java
@@ -2,6 +2,7 @@ package StepDefinition;
 
 import org.junit.runner.RunWith;
 
+
 import io.cucumber.junit.CucumberOptions;
 
 import io.cucumber.junit.Cucumber;
@@ -10,7 +11,9 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(features="src/test/resources/Feature",glue= {"StepDefinition"},monochrome =true,
 //plugin={"pretty","html:target/htmlReports/report.html"},
 tags="@logintag"		)
-public class TestRunner {
+public class RunnerTest {
+	
+	
 
 }
 
